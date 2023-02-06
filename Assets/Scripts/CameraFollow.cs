f66db7dfe4c5d10dc142d3dec92ad3f34b1a5883
yumeShinoA <zlee3@uci.edu>
2023-02-05 17:35:01 -0800

    Added Camera Follow Player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+}
