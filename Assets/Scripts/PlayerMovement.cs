3bc1aa7471c2bfb4e1c47db2b2ac663f688e8abe
mr0hoverboard1 <48263162+mr0hoverboard1@users.noreply.github.com>
2023-02-06 18:42:29 -0800

    Added Dash and Projectile
    Added a way for the player to dash in PlayerMovement and updated the Projectile Prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,15 +11,57 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    private float activeMoveSpeed;
+    public float dashSpeed;
+    public float dashLength = .5f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+    private bool dash = false;
+
+    private void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+    }
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.Space) && dashCoolCounter <= 0)
+        {
+            dash = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * activeMoveSpeed * Time.fixedDeltaTime);
+        if(dash)
+        {
+            if(dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+                dash = false;
+            }
+        }
+        if(dashCounter > 0)
+        {
+            dashCounter -= Time.fixedDeltaTime;
+
+            if(dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.fixedDeltaTime;
+        }
     }
 }
