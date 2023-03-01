7a4b389f68469920b0d52111b60f50efc44947fe
mr0hoverboard1 <48263162+mr0hoverboard1@users.noreply.github.com>
2023-03-01 10:50:13 -0800

    Multiplayer Scene and Camera
    Created a Multiplayer scene which is a copy of the singleplayer scene but with two humans. Also changed the camera so that it works in both singleplayer and multiplayer

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,11 +2,77 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    [SerializeField] private Transform player;
+    [SerializeField] public Transform[] players;
+    public float DampTime = 0.2f;
+    public float ScreenEdgeBuffer = 4f;
+    public float MinSize = 6.5f;
+
+    private Camera m_Camera;
+    private Vector3 DesiredPosition;
+    private float ZoomSpeed;
+    private Vector3 MoveVelocity;
+
+    private void Awake()
+    {
+        m_Camera = GetComponent<Camera>();
+    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 1, -5);
+        Move();
+
+        Zoom();
+        //transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+
+    private void Move()
+    {
+        FindAveragePosition();
+
+        transform.position = Vector3.SmoothDamp(transform.position, DesiredPosition, ref MoveVelocity, DampTime);
+    }
+
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
+
+        for(int i = 0; i < players.Length; i++)
+        {
+            if (!players[i].gameObject.activeSelf)
+                continue;
+            averagePos += players[i].position;
+            numTargets++;
+        }
+        if (numTargets > 0)
+            averagePos /= numTargets;
+
+        averagePos.z = transform.position.z;
+        DesiredPosition = averagePos;
+    }
+
+    private void Zoom()
+    {
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref ZoomSpeed, DampTime);
+    }
+
+    private float FindRequiredSize()
+    {
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(DesiredPosition);
+        float size = 0f;
+        for(int i = 0; i < players.Length; i++)
+        {
+            if (!players[i].gameObject.activeSelf)
+                continue;
+            Vector3 targetLocalPos = transform.InverseTransformPoint(players[i].position);
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
+        }
+        size += ScreenEdgeBuffer;
+        size = Mathf.Max(size, MinSize);
+        return size;
     }
 }
